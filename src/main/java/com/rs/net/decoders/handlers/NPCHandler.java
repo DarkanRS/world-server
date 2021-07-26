83b05958f70d0bf30eeba2eead9e7f56dfbdebff
jesse <jesseguerrero1991@gmail.com>
2021-07-26 11:13:59 -0500

    Gave animations to ice elemental & added combat defs to examine

--- src/main/java/com/rs/net/decoders/handlers/NPCHandler.java
+++ src/main/java/com/rs/net/decoders/handlers/NPCHandler.java
@@ -76,12 +76,15 @@ import com.rs.utils.shop.ShopsHandler;
 public class NPCHandler {
 
 	public static void handleExamine(final Player player, final NPC npc) {
-		if (player.hasRights(Rights.DEVELOPER)) {
-			player.sendMessage("NPC - [id=" + npc.getId() + ", loc=[" + npc.getX() + ", " + npc.getY() + ", " + npc.getPlane() + "]]. " + npc.getDefinitions().respawnDirection);
-			if (npc.getDefinitions().transformTo != null) {
-				player.sendMessage(npc.getDefinitions().getConfigInfoString());
-			}
-		}
+        if (player.hasRights(Rights.DEVELOPER)) {
+            player.sendMessage("NPC - [id=" + npc.getId() + ", loc=[" + npc.getX() + ", " + npc.getY() + ", " + npc.getPlane() + "]]. " + npc.getDefinitions().respawnDirection);
+            player.sendMessage("HP: " + npc.getMaxHitpoints() + " Crush Def: " + npc.getDefinitions().getCrushDef() + " Slash Def: " +
+                    npc.getDefinitions().getSlashDef() + " Stab Def: " + npc.getDefinitions().getStabDef() + " Range Def: "+ npc.getDefinitions().getRangeDef() +
+                    " Mage Def: " + npc.getDefinitions().getMagicDef());
+            if (npc.getDefinitions().transformTo != null) {
+                player.sendMessage(npc.getDefinitions().getConfigInfoString());
+            }
+        }
 		player.getPackets().sendNPCMessage(player, 0, 0xFFFFFF, npc, NPCExamines.getExamine(npc, player) + " ("+npc.getId()+")");
 		if (npc.getDefinitions().hasAttackOption() || npc.getDefinitions().hasOption("Investigate")) {
 			player.sendOptionDialogue("Would you like to check the drops on this monster?", new String[] { "Show drops (1,000 kills)", "Show drops (5,000 kills)", "Show drops (50,000 kills)", "Nevermind"}, new DialogueOptionEvent() {
