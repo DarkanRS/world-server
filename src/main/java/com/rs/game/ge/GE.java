f19663662d31eb1bfdd50f5295813ae51c15d8bf
Trent <trenton.kress@gmail.com>
2021-09-01 13:14:17 -0700

    Reminder notes

--- src/main/java/com/rs/game/ge/GE.java
+++ src/main/java/com/rs/game/ge/GE.java
@@ -26,6 +26,7 @@ import com.rs.utils.ItemExamines;
 
 @PluginEventHandler
 public class GE {
+	//TODO change streams for updating players super super easy
 
 	private static final int OFFER_SELECTION = 105;
 	private static final int DEPOSIT_INV = 107;
