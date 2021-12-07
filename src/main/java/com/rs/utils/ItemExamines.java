34ef10540068dfc11aa0f2b8dc04be3503655d9a
Trent <trenton.kress@gmail.com>
2021-12-07 10:41:26 -0800

    Refactor item weight and examines to organized JSON format

--- src/main/java/com/rs/utils/ItemExamines.java
+++ src/main/java/com/rs/utils/ItemExamines.java
@@ -16,38 +16,29 @@
 //
 package com.rs.utils;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileChannel.MapMode;
 import java.util.HashMap;
-
+import java.util.Map;
+import com.google.gson.JsonIOException;
+import com.google.gson.reflect.TypeToken;
 import com.rs.Settings;
+import com.rs.lib.file.JsonFileManager;
 import com.rs.lib.game.Item;
-import com.rs.lib.util.Logger;
 import com.rs.plugin.annotations.PluginEventHandler;
 import com.rs.plugin.annotations.ServerStartupEvent;
 
 @PluginEventHandler
 public class ItemExamines {
-
-	private final static HashMap<Integer, String> itemExamines = new HashMap<Integer, String>();
-	private final static String PACKED_PATH = "data/items/packedExamines.e";
-	private final static String UNPACKED_PATH = "data/items/unpackedExamines.txt";
+	private static Map<Integer, String> EXAMINES = new HashMap<>();
+	private final static String PATH = "./data/items/examines.json";
 
 	@ServerStartupEvent
-	public static final void init() {
-		if (new File(PACKED_PATH).exists())
-			loadPackedItemExamines();
+	public static final void init() throws JsonIOException, IOException {
+		if (new File(PATH).exists())
+			EXAMINES = JsonFileManager.loadJsonFile(new File(PATH), new TypeToken<Map<Integer, String>>(){}.getType());
 		else
-			loadUnpackedItemExamines();
+			System.err.println("No item examines file found at " + PATH + "!");
 	}
 
 	public static final String getExamine(Item item) {
@@ -55,70 +46,9 @@ public class ItemExamines {
 			return item.getAmount() + " x " + item.getDefinitions().getName() + ".";
 		if (item.getDefinitions().isNoted())
 			return "Swap this note at any bank for the equivalent item.";
-		String examine = itemExamines.get(item.getId());
+		String examine = EXAMINES.get(item.getId());
 		if (examine != null)
 			return examine + (Settings.getConfig().isDebug() ? " Tradable: " + item.getDefinitions().canExchange() : "");
 		return "It's an " + item.getDefinitions().getName() + "." + (Settings.getConfig().isDebug() ? " Tradable: " + item.getDefinitions().canExchange() : "");
-	}
-
-	private static void loadPackedItemExamines() {
-		try {
-			RandomAccessFile in = new RandomAccessFile(PACKED_PATH, "r");
-			FileChannel channel = in.getChannel();
-			ByteBuffer buffer = channel.map(MapMode.READ_ONLY, 0, channel.size());
-			while (buffer.hasRemaining())
-				itemExamines.put(buffer.getShort() & 0xffff, readAlexString(buffer));
-			channel.close();
-			in.close();
-		} catch (Throwable e) {
-			Logger.handle(e);
-		}
-	}
-
-	@SuppressWarnings("resource")
-	private static void loadUnpackedItemExamines() {
-		Logger.log("ItemExamines", "Packing item examines...");
-		try {
-			BufferedReader in = new BufferedReader(new FileReader(UNPACKED_PATH));
-			DataOutputStream out = new DataOutputStream(new FileOutputStream(PACKED_PATH));
-			while (true) {
-				String line = in.readLine();
-				if (line == null)
-					break;
-				if (line.startsWith("//"))
-					continue;
-				line = line.replace("﻿", "");
-				String[] splitedLine = line.split(" - ", 2);
-				if (splitedLine.length < 2)
-					throw new RuntimeException("Invalid list for item examine line: " + line);
-				int itemId = Integer.valueOf(splitedLine[0]);
-				if (splitedLine[1].length() > 255)
-					continue;
-				out.writeShort(itemId);
-				writeAlexString(out, splitedLine[1]);
-				itemExamines.put(itemId, splitedLine[1]);
-			}
-			in.close();
-			out.flush();
-			out.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	public static String readAlexString(ByteBuffer buffer) {
-		int count = buffer.get() & 0xff;
-		byte[] bytes = new byte[count];
-		buffer.get(bytes, 0, count);
-		return new String(bytes);
-	}
-
-	public static void writeAlexString(DataOutputStream out, String string) throws IOException {
-		byte[] bytes = string.getBytes();
-		out.writeByte(bytes.length);
-		out.write(bytes);
 	}
 }
--- src/main/java/com/rs/utils/ItemWeights.java
+++ src/main/java/com/rs/utils/ItemWeights.java
@@ -16,118 +16,42 @@
 //
 package com.rs.utils;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileChannel.MapMode;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-
+import java.util.Map;
+import com.google.gson.JsonIOException;
+import com.google.gson.reflect.TypeToken;
+import com.rs.lib.file.JsonFileManager;
 import com.rs.lib.game.Item;
-import com.rs.lib.util.Logger;
 import com.rs.plugin.annotations.PluginEventHandler;
 import com.rs.plugin.annotations.ServerStartupEvent;
 
 @PluginEventHandler
 public class ItemWeights {
 
-	private final static HashMap<Integer, Double> itemWeights = new HashMap<Integer, Double>();
-	private final static String PACKED_PATH = "data/items/packedWeights.dat";
-	private final static String UNPACKED_PATH = "data/items/unpackedWeights.txt";
-
-	private static final Set<Integer> NEGATIVE_WEIGHT_ITEMS = new HashSet<>(Arrays.asList(new Integer[] { 88, 10553, 10069, 10071, 24210, 24208, 24206, 14936, 14938, 24560, 24561, 24562, 24563, 24564 }));
-
+	private static Map<Integer, Double> WEIGHTS = new HashMap<>();
+	private final static String PATH = "./data/items/weight.json";
+	
 	@ServerStartupEvent
-	public static final void init() {
-		if (new File(PACKED_PATH).exists())
-			loadPackedItemWeights();
+	public static final void init() throws JsonIOException, IOException {
+		if (new File(PATH).exists())
+			WEIGHTS = JsonFileManager.loadJsonFile(new File(PATH), new TypeToken<Map<Integer, Double>>(){}.getType());
 		else
-			loadUnpackedItemWeights();
+			System.err.println("No item weight file found at " + PATH + "!");
 	}
 
 	public static final double getWeight(Item item, boolean equipped) {
 		if (item.getDefinitions().isNoted())
 			return 0;
-		Double weight = itemWeights.get(item.getId());
+		Double weight = WEIGHTS.get(item.getId());
 		if (weight == null)
 			return 0;
-		if (NEGATIVE_WEIGHT_ITEMS.contains(item.getId())) {
+		if (weight < 0.0) {
 			if (equipped)
 				return -weight;
 			return 0;
 		}
 		return weight;
-	}
-
-	private static void loadPackedItemWeights() {
-		try {
-			RandomAccessFile in = new RandomAccessFile(PACKED_PATH, "r");
-			FileChannel channel = in.getChannel();
-			ByteBuffer buffer = channel.map(MapMode.READ_ONLY, 0, channel.size());
-			while (buffer.hasRemaining()) {
-				itemWeights.put(buffer.getShort() & 0xffff, buffer.getDouble());
-			}
-			channel.close();
-			in.close();
-		} catch (Throwable e) {
-			Logger.handle(e);
-		}
-	}
-
-	private static void loadUnpackedItemWeights() {
-		Logger.log(ItemWeights.class, "Packing item weights...");
-		try {
-			BufferedReader in = new BufferedReader(new FileReader(UNPACKED_PATH));
-			@SuppressWarnings("resource")
-			DataOutputStream out = new DataOutputStream(new FileOutputStream(PACKED_PATH));
-			while (true) {
-				String line = in.readLine();
-				if (line == null)
-					break;
-				if (line.startsWith("//"))
-					continue;
-				line = line.replace("﻿", "");
-				String[] splitedLine = line.split(" - ", 2);
-				if (splitedLine.length < 2) {
-					in.close();
-					throw new RuntimeException("Invalid list for item weight line: " + line);
-				}
-				int itemId = Integer.valueOf(splitedLine[0]);
-				double weight = Double.valueOf(splitedLine[1]);
-				out.writeShort(itemId);
-				out.writeDouble(weight);
-				itemWeights.put(itemId, weight);
-			}
-			in.close();
-			out.flush();
-			out.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	public static String readAlexString(ByteBuffer buffer) {
-		int count = buffer.get() & 0xff;
-		byte[] bytes = new byte[count];
-		buffer.get(bytes, 0, count);
-		return new String(bytes);
-	}
-
-	public static void writeAlexString(DataOutputStream out, String string) throws IOException {
-		byte[] bytes = string.getBytes();
-		out.writeByte(bytes.length);
-		out.write(bytes);
 	}
 }
