2f52c798e0381beee06824b3dd3e2635c5c6684e
Jesse Guerrero <jesseguerrero1991@gmail.com>
2021-12-27 21:31:47 -0600

    -Finally found Dragon Slayer cutscene musics... -Upgraded rest musicians to include its own action and music per instrument. -Corrected a few musicians as well.

--- src/main/java/com/rs/game/player/managers/MusicsManager.java
+++ src/main/java/com/rs/game/player/managers/MusicsManager.java
@@ -34,201 +34,201 @@ import com.rs.utils.music.Song;
 @PluginEventHandler
 public final class MusicsManager {
 
-	private static final int[] CONFIG_IDS = new int[] { 20, 21, 22, 23, 24, 25, 298, 311, 346, 414, 464, 598, 662, 721, 906, 1009, 1104, 1136, 1180, 1202, 1381, 1394, 1434, 1596, 1618, 1619, 1620, 1865, 1864, 2246, 2019, -1, 2430, 2559 };
-	private static final int[] PLAY_LIST_CONFIG_IDS = new int[] { 1621, 1622, 1623, 1624, 1625, 1626 };
+    private static final int[] CONFIG_IDS = new int[]{20, 21, 22, 23, 24, 25, 298, 311, 346, 414, 464, 598, 662, 721, 906, 1009, 1104, 1136, 1180, 1202, 1381, 1394, 1434, 1596, 1618, 1619, 1620, 1865, 1864, 2246, 2019, -1, 2430, 2559};
+    private static final int[] PLAY_LIST_CONFIG_IDS = new int[]{1621, 1622, 1623, 1624, 1625, 1626};
 
-	private transient Player player;
-	private transient int playingMusic;
-	private transient long playingMusicDelay;
-	private transient boolean settedMusic;
-	private ArrayList<Integer> unlockedMusics;
-	private ArrayList<Integer> playList;
+    private transient Player player;
+    private transient int playingMusic;
+    private transient long playingMusicDelay;
+    private transient boolean settedMusic;
+    private ArrayList<Integer> unlockedMusics;
+    private ArrayList<Integer> playList;
 
-	private transient boolean playListOn;
-	private transient int nextPlayListMusic;
-	private transient boolean shuffleOn;
+    private transient boolean playListOn;
+    private transient int nextPlayListMusic;
+    private transient boolean shuffleOn;
 
-	public MusicsManager() {
-		unlockedMusics = new ArrayList<Integer>();
-		playList = new ArrayList<Integer>(12);
-		// auto unlocked musics
-		unlockedMusics.add(62);
-		unlockedMusics.add(400);
-		unlockedMusics.add(16);
-		unlockedMusics.add(466);
-		unlockedMusics.add(321);
-		unlockedMusics.add(547);
-		unlockedMusics.add(621);
-		unlockedMusics.add(207);
-		unlockedMusics.add(401);
-		unlockedMusics.add(147);
-		unlockedMusics.add(457);
-		unlockedMusics.add(552);
-		unlockedMusics.add(858);
-	}
-	
-	public static ButtonClickHandler handlePlaylistButtons = new ButtonClickHandler(187) {
-		@Override
-		public void handle(ButtonClickEvent e) {
-			if (e.getComponentId() == 1) {
-				if (e.getPacket() == ClientPacket.IF_OP1)
-					e.getPlayer().getMusicsManager().playAnotherMusic(e.getSlotId() / 2);
-				else if (e.getPacket() == ClientPacket.IF_OP2)
-					e.getPlayer().getMusicsManager().sendHint(e.getSlotId() / 2);
-				else if (e.getPacket() == ClientPacket.IF_OP3)
-					e.getPlayer().getMusicsManager().addToPlayList(e.getSlotId() / 2);
-				else if (e.getPacket() == ClientPacket.IF_OP4)
-					e.getPlayer().getMusicsManager().removeFromPlayList(e.getSlotId() / 2);
-			} else if (e.getComponentId() == 4)
-				e.getPlayer().getMusicsManager().addPlayingMusicToPlayList();
-			else if (e.getComponentId() == 10)
-				e.getPlayer().getMusicsManager().switchPlayListOn();
-			else if (e.getComponentId() == 11)
-				e.getPlayer().getMusicsManager().clearPlayList();
-			else if (e.getComponentId() == 13)
-				e.getPlayer().getMusicsManager().switchShuffleOn();
-		}
-	};
-	
-	public void passMusics(Player p) {
-		for (int musicId : p.getMusicsManager().unlockedMusics) {
-			if (!unlockedMusics.contains(musicId))
-				unlockedMusics.add(musicId);
-		}
-	}
+    public MusicsManager() {
+        unlockedMusics = new ArrayList<Integer>();
+        playList = new ArrayList<Integer>(12);
+        // auto unlocked musics
+        unlockedMusics.add(62);
+        unlockedMusics.add(400);
+        unlockedMusics.add(16);
+        unlockedMusics.add(466);
+        unlockedMusics.add(321);
+        unlockedMusics.add(547);
+        unlockedMusics.add(621);
+        unlockedMusics.add(207);
+        unlockedMusics.add(401);
+        unlockedMusics.add(147);
+        unlockedMusics.add(457);
+        unlockedMusics.add(552);
+        unlockedMusics.add(858);
+    }
 
-	public boolean hasMusic(int id) {
-		return unlockedMusics.contains(id);
-	}
+    public static ButtonClickHandler handlePlaylistButtons = new ButtonClickHandler(187) {
+        @Override
+        public void handle(ButtonClickEvent e) {
+            if (e.getComponentId() == 1) {
+                if (e.getPacket() == ClientPacket.IF_OP1)
+                    e.getPlayer().getMusicsManager().playAnotherMusic(e.getSlotId() / 2);
+                else if (e.getPacket() == ClientPacket.IF_OP2)
+                    e.getPlayer().getMusicsManager().sendHint(e.getSlotId() / 2);
+                else if (e.getPacket() == ClientPacket.IF_OP3)
+                    e.getPlayer().getMusicsManager().addToPlayList(e.getSlotId() / 2);
+                else if (e.getPacket() == ClientPacket.IF_OP4)
+                    e.getPlayer().getMusicsManager().removeFromPlayList(e.getSlotId() / 2);
+            } else if (e.getComponentId() == 4)
+                e.getPlayer().getMusicsManager().addPlayingMusicToPlayList();
+            else if (e.getComponentId() == 10)
+                e.getPlayer().getMusicsManager().switchPlayListOn();
+            else if (e.getComponentId() == 11)
+                e.getPlayer().getMusicsManager().clearPlayList();
+            else if (e.getComponentId() == 13)
+                e.getPlayer().getMusicsManager().switchShuffleOn();
+        }
+    };
 
-	public void setPlayer(Player player) {
-		this.player = player;
-		playingMusic = World.getRegion(player.getRegionId()).getMusicId();
-	}
+    public void passMusics(Player p) {
+        for (int musicId : p.getMusicsManager().unlockedMusics) {
+            if (!unlockedMusics.contains(musicId))
+                unlockedMusics.add(musicId);
+        }
+    }
 
-	public void switchShuffleOn() {
-		if (shuffleOn) {
-			playListOn = false;
-			refreshPlayListConfigs();
-		}
-		shuffleOn = !shuffleOn;
-	}
+    public boolean hasMusic(int id) {
+        return unlockedMusics.contains(id);
+    }
 
-	public void switchPlayListOn() {
-		if (playListOn) {
-			playListOn = false;
-			shuffleOn = false;
-			refreshPlayListConfigs();
-		} else {
-			playListOn = true;
-			nextPlayListMusic = 0;
-			replayMusic();
-		}
-	}
+    public void setPlayer(Player player) {
+        this.player = player;
+        playingMusic = World.getRegion(player.getRegionId()).getMusicId();
+    }
 
-	public void clearPlayList() {
-		if (playList.isEmpty())
-			return;
-		playList.clear();
-		refreshPlayListConfigs();
-	}
+    public void switchShuffleOn() {
+        if (shuffleOn) {
+            playListOn = false;
+            refreshPlayListConfigs();
+        }
+        shuffleOn = !shuffleOn;
+    }
 
-	public void addPlayingMusicToPlayList() {
-		addToPlayList((int) EnumDefinitions.getEnum(1351).getKeyForValue(playingMusic));
-	}
+    public void switchPlayListOn() {
+        if (playListOn) {
+            playListOn = false;
+            shuffleOn = false;
+            refreshPlayListConfigs();
+        } else {
+            playListOn = true;
+            nextPlayListMusic = 0;
+            replayMusic();
+        }
+    }
 
-	public void addToPlayList(int musicIndex) {
-		if (playList.size() == 12)
-			return;
-		int musicId = EnumDefinitions.getEnum(1351).getIntValue(musicIndex);
-		if (musicId != -1 && unlockedMusics.contains(musicId) && !playList.contains(musicId)) {
-			playList.add(musicId);
-			if (playListOn)
-				switchPlayListOn();
-			else
-				refreshPlayListConfigs();
-		}
-	}
+    public void clearPlayList() {
+        if (playList.isEmpty())
+            return;
+        playList.clear();
+        refreshPlayListConfigs();
+    }
 
-	public void removeFromPlayList(int musicIndex) {
-		Integer musicId = EnumDefinitions.getEnum(1351).getIntValue(musicIndex);
-		if (musicId != -1 && unlockedMusics.contains(musicId) && playList.contains(musicId)) {
-			playList.remove(musicId);
-			if (playListOn)
-				switchPlayListOn();
-			else
-				refreshPlayListConfigs();
-		}
-	}
+    public void addPlayingMusicToPlayList() {
+        addToPlayList((int) EnumDefinitions.getEnum(1351).getKeyForValue(playingMusic));
+    }
 
-	public void refreshPlayListConfigs() {
-		int[] configValues = new int[PLAY_LIST_CONFIG_IDS.length];
-		for (int i = 0; i < configValues.length; i++)
-			configValues[i] = -1;
-		for (int i = 0; i < playList.size(); i += 2) {
-			Integer musicId1 = playList.get(i);
-			Integer musicId2 = (i + 1) >= playList.size() ? null : playList.get(i + 1);
-			if (musicId1 == null && musicId2 == null)
-				break;
-			int musicIndex = (int) EnumDefinitions.getEnum(1351).getKeyForValue(musicId1);
-			int configValue;
-			if (musicId2 != null) {
-				int musicIndex2 = (int) EnumDefinitions.getEnum(1351).getKeyForValue(musicId2);
-				configValue = musicIndex | musicIndex2 << 15;
-			} else
-				configValue = musicIndex | -1 << 15;
-			configValues[i / 2] = configValue;
-		}
-		for (int i = 0; i < PLAY_LIST_CONFIG_IDS.length; i++)
-			if (PLAY_LIST_CONFIG_IDS[i] == -1)
-				player.getVars().setVar(PLAY_LIST_CONFIG_IDS[i], configValues[i]);
-	}
+    public void addToPlayList(int musicIndex) {
+        if (playList.size() == 12)
+            return;
+        int musicId = EnumDefinitions.getEnum(1351).getIntValue(musicIndex);
+        if (musicId != -1 && unlockedMusics.contains(musicId) && !playList.contains(musicId)) {
+            playList.add(musicId);
+            if (playListOn)
+                switchPlayListOn();
+            else
+                refreshPlayListConfigs();
+        }
+    }
 
-	public void refreshListConfigs() {
-		int[] configValues = new int[CONFIG_IDS.length];
-		for (int musicId : unlockedMusics) {
-			int musicIndex = (int) EnumDefinitions.getEnum(1351).getKeyForValue(musicId);
-			if (musicIndex == -1)
-				continue;
-			int index = getConfigIndex(musicIndex);
-			if (index >= CONFIG_IDS.length)
-				continue;
-			configValues[index] |= 1 << (musicIndex - (index * 32));
-		}
-		for (int i = 0; i < CONFIG_IDS.length; i++) {
-			if (CONFIG_IDS[i] != -1 && configValues[i] != 0)
-				player.getVars().setVar(CONFIG_IDS[i], configValues[i]);
-		}
-	}
+    public void removeFromPlayList(int musicIndex) {
+        Integer musicId = EnumDefinitions.getEnum(1351).getIntValue(musicIndex);
+        if (musicId != -1 && unlockedMusics.contains(musicId) && playList.contains(musicId)) {
+            playList.remove(musicId);
+            if (playListOn)
+                switchPlayListOn();
+            else
+                refreshPlayListConfigs();
+        }
+    }
 
-	public void addMusic(int musicId) {
-		unlockedMusics.add(musicId);
-		refreshListConfigs();
-	}
-	
-	public int unlockedMusicCount() {
-		return unlockedMusics.size();
-	}
+    public void refreshPlayListConfigs() {
+        int[] configValues = new int[PLAY_LIST_CONFIG_IDS.length];
+        for (int i = 0; i < configValues.length; i++)
+            configValues[i] = -1;
+        for (int i = 0; i < playList.size(); i += 2) {
+            Integer musicId1 = playList.get(i);
+            Integer musicId2 = (i + 1) >= playList.size() ? null : playList.get(i + 1);
+            if (musicId1 == null && musicId2 == null)
+                break;
+            int musicIndex = (int) EnumDefinitions.getEnum(1351).getKeyForValue(musicId1);
+            int configValue;
+            if (musicId2 != null) {
+                int musicIndex2 = (int) EnumDefinitions.getEnum(1351).getKeyForValue(musicId2);
+                configValue = musicIndex | musicIndex2 << 15;
+            } else
+                configValue = musicIndex | -1 << 15;
+            configValues[i / 2] = configValue;
+        }
+        for (int i = 0; i < PLAY_LIST_CONFIG_IDS.length; i++)
+            if (PLAY_LIST_CONFIG_IDS[i] == -1)
+                player.getVars().setVar(PLAY_LIST_CONFIG_IDS[i], configValues[i]);
+    }
 
-	public int getConfigIndex(int musicId) {
-		return (musicId + 1) / 32;
-	}
+    public void refreshListConfigs() {
+        int[] configValues = new int[CONFIG_IDS.length];
+        for (int musicId : unlockedMusics) {
+            int musicIndex = (int) EnumDefinitions.getEnum(1351).getKeyForValue(musicId);
+            if (musicIndex == -1)
+                continue;
+            int index = getConfigIndex(musicIndex);
+            if (index >= CONFIG_IDS.length)
+                continue;
+            configValues[index] |= 1 << (musicIndex - (index * 32));
+        }
+        for (int i = 0; i < CONFIG_IDS.length; i++) {
+            if (CONFIG_IDS[i] != -1 && configValues[i] != 0)
+                player.getVars().setVar(CONFIG_IDS[i], configValues[i]);
+        }
+    }
 
-	public void unlockMusicPlayer() {
-		player.getPackets().setIFRightClickOps(187, 1, 0, CONFIG_IDS.length * 64, 0, 1, 2, 3);
-	}
+    public void addMusic(int musicId) {
+        unlockedMusics.add(musicId);
+        refreshListConfigs();
+    }
 
-	public void init() {
-		// unlock music inter all options
-		if (playingMusic >= 0)
-			playMusic(playingMusic);
-		refreshListConfigs();
-		refreshPlayListConfigs();
-	}
+    public int unlockedMusicCount() {
+        return unlockedMusics.size();
+    }
 
-	public boolean musicEnded() {
-		return playingMusic != -2 && playingMusicDelay + (180000) < System.currentTimeMillis();
-	}
+    public int getConfigIndex(int musicId) {
+        return (musicId + 1) / 32;
+    }
+
+    public void unlockMusicPlayer() {
+        player.getPackets().setIFRightClickOps(187, 1, 0, CONFIG_IDS.length * 64, 0, 1, 2, 3);
+    }
+
+    public void init() {
+        // unlock music inter all options
+        if (playingMusic >= 0)
+            playMusic(playingMusic);
+        refreshListConfigs();
+        refreshPlayListConfigs();
+    }
+
+    public boolean musicEnded() {
+        return playingMusic != -2 && playingMusicDelay + (180000) < System.currentTimeMillis();
+    }
 
     public void replayMusic() {
         if (playListOn && playList.size() > 0) {
@@ -240,7 +240,7 @@ public final class MusicsManager {
                 playingMusic = playList.get(nextPlayListMusic++);
             }
         } else if (unlockedMusics.size() > 0) {// random music
-            if(player.getDungManager().isInsideDungeon())
+            if (player.getDungManager().isInsideDungeon())
                 playingMusic = unlockedMusics.get(Utils.getRandomInclusive(unlockedMusics.size() - 1));
             else
                 do {
@@ -250,19 +250,27 @@ public final class MusicsManager {
         playMusic(playingMusic);
     }
 
-	public void checkMusic(int requestMusicId) {
-		if (playListOn || settedMusic && playingMusicDelay + (180000) >= System.currentTimeMillis())
-			return;
-		settedMusic = false;
-		if (playingMusic != requestMusicId)
-			playMusic(requestMusicId);
-	}
+    /**
+     * Plays music if checked music is not on but includes hints.
+     *
+     * @param requestMusicId
+     */
+    public void checkMusic(int requestMusicId) {
+        if (playListOn || settedMusic && playingMusicDelay + (180000) >= System.currentTimeMillis())
+            return;
+        settedMusic = false;
+        if (playingMusic != requestMusicId)
+            playMusic(requestMusicId);
+    }
 
 	public void forcePlayMusic(int musicId) {
 		settedMusic = true;
 		playMusic(musicId);
 	}
 
+    public boolean isPlaying(int requestMusicId) {
+        return playingMusic == requestMusicId;
+    }
 	public void reset() {
 		settedMusic = false;
 		player.getMusicsManager().checkMusic(World.getRegion(player.getRegionId()).getMusicId());
