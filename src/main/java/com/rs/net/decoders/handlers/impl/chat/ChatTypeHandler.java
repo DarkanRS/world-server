b68ef4cc806b1d24fe9925032f4fb5248d785284
Trent <trenton.kress@gmail.com>
2022-01-12 13:40:50 -0800

    Fix chat type bug

--- src/main/java/com/rs/net/decoders/handlers/impl/chat/ChatTypeHandler.java
+++ src/main/java/com/rs/net/decoders/handlers/impl/chat/ChatTypeHandler.java
@@ -19,12 +19,10 @@ package com.rs.net.decoders.handlers.impl.chat;
 import com.rs.game.player.Player;
 import com.rs.lib.net.packets.PacketHandler;
 import com.rs.lib.net.packets.decoders.chat.ChatType;
-import com.rs.net.LobbyCommunicator;
 
 public class ChatTypeHandler implements PacketHandler<Player, ChatType> {
 	@Override
 	public void handle(Player player, ChatType packet) {
 		player.chatType = packet.getType();
-		LobbyCommunicator.forwardPackets(player, packet);
 	}
 }
