457304bc70d632c3330faac1f845e88a0d88ab20
Jesse Guerrero <jesseguerrero1991@gmail.com>
2022-02-19 20:45:18 -0600

    -Added a champions guild shop for black full helm. You need it for Heroes Quest...

--- src/main/java/com/rs/game/player/content/world/regions/Varrock.java
+++ src/main/java/com/rs/game/player/content/world/regions/Varrock.java
@@ -642,5 +642,36 @@ public class Varrock {
 		}
 	};
 
+	public static NPCClickHandler handleValaineChampsGuild = new NPCClickHandler(new Object[] { 536 }) {
+		@Override
+		public void handle(NPCClickEvent e) {
+			int NPC = e.getNPCId();
+			if(e.getOption().equalsIgnoreCase("talk-to"))
+				e.getPlayer().startConversation(new Dialogue()
+						.addNPC(NPC, HeadE.CALM_TALK, "Hello there. Want to have a look at what we're selling today?")
+						.addOptions("Choose an option:", new Options() {
+						@Override
+						public void create() {
+							option("Yes, please.", new Dialogue()
+									.addPlayer(HeadE.HAPPY_TALKING, "Yes, please.")
+									.addNext(()->{ShopsHandler.openShop(e.getPlayer(), "valaines_shop_of_champions");})
+							);
+							option("How should I use your shop?", new Dialogue()
+									.addPlayer(HeadE.HAPPY_TALKING, "How should I use your shop?")
+									.addNPC(NPC, HeadE.CALM_TALK, "I'm glad you ask! You can buy as many of the items stocked as you wish. You can also sell most items to the shop.")
+									.addNext(()->{ShopsHandler.openShop(e.getPlayer(), "valaines_shop_of_champions");})
+							);
+							option("No, thank you.", new Dialogue()
+									.addPlayer(HeadE.HAPPY_TALKING, "No, thank you.")
+									.addNPC(NPC, HeadE.CALM_TALK, "Well, alright.")
+							);
+						}
+					})
+				);
+			if(e.getOption().equalsIgnoreCase("trade"))
+				ShopsHandler.openShop(e.getPlayer(), "valaines_shop_of_champions");
+		}
+	};
+
 
 }
