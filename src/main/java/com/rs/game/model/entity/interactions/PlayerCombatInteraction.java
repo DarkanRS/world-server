ad35a68895d0d8d8f946db989754ef9a1e8a70c4
Trenton Kress <trenton.kress@gmail.com>
2022-03-23 07:45:22 -0700

    Cancel combat on interaction removal

--- src/main/java/com/rs/game/model/entity/interactions/PlayerCombatInteraction.java
+++ src/main/java/com/rs/game/model/entity/interactions/PlayerCombatInteraction.java
@@ -2,6 +2,7 @@ package com.rs.game.model.entity.interactions;
 
 import com.rs.game.content.combat.PlayerCombat;
 import com.rs.game.model.entity.Entity;
+import com.rs.game.model.entity.npc.NPC;
 import com.rs.game.model.entity.player.Player;
 import com.rs.game.model.entity.player.interactions.PlayerEntityInteractionAction;
 
@@ -19,7 +20,7 @@ public class PlayerCombatInteraction extends PlayerEntityInteractionAction<Playe
 
 	@Override
 	public boolean checkAll(Player player) {
-		if (target.isDead())
+		if (target.isDead() || (target instanceof NPC n && n.isCantInteract()))
 			return false;
 		setDistance(PlayerCombat.getAttackRange(player));
 		player.setNextFaceEntity(target);
