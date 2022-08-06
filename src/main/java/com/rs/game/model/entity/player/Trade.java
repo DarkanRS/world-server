c8e5ad09bb35ed5d7d51fbaf41bead89d351d0de
Trenton Kress <trenton.kress@gmail.com>
2022-08-06 10:42:56 -0700

    Enable untradeable item trade

--- src/main/java/com/rs/game/model/entity/player/Trade.java
+++ src/main/java/com/rs/game/model/entity/player/Trade.java
@@ -144,11 +144,13 @@ public class Trade {
 				if (item == null)
 					return;
 				Item[] itemsBefore = items.getItemsCopy();
-				int maxAmount = items.getNumberOf(item);
-				if (amount < maxAmount)
-					item = new Item(item.getId(), amount);
-				else
-					item = new Item(item.getId(), maxAmount);
+				if (!item.containsMetaData()) {
+					int maxAmount = items.getNumberOf(item);
+					if (amount < maxAmount)
+						item = new Item(item.getId(), amount);
+					else
+						item = new Item(item.getId(), maxAmount);
+				}
 				items.remove(slot, item);
 				player.getInventory().addItem(item);
 				refreshItems(itemsBefore);
@@ -190,11 +192,13 @@ public class Trade {
 					return;
 				}
 				Item[] itemsBefore = items.getItemsCopy();
-				int maxAmount = player.getInventory().getItems().getNumberOf(item);
-				if (amount < maxAmount)
-					item = new Item(item.getId(), amount);
-				else
-					item = new Item(item.getId(), maxAmount);
+				if (!item.containsMetaData()) {
+					int maxAmount = player.getInventory().getItems().getNumberOf(item);
+					if (amount < maxAmount)
+						item = new Item(item.getId(), amount);
+					else
+						item = new Item(item.getId(), maxAmount);
+				}
 				items.add(item);
 				player.getInventory().deleteItem(slot, item);
 				refreshItems(itemsBefore);
