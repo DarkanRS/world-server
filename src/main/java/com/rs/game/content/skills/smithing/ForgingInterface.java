e9a0d276b1694db944fd66c018216199f47d6455
Trenton Kress <trenton.kress@gmail.com>
2022-08-08 16:03:12 -0700

    Completely re-code smithing. Fix a varc string math miscalculation resulting in strings of exactly 252 characters in length (conveniently the length of the stats string for comparing a proselyte chest to a monk robe chest) sending too much data in a smaller sized packet

--- src/main/java/com/rs/game/content/skills/smithing/ForgingInterface.java
+++ src/main/java/com/rs/game/content/skills/smithing/ForgingInterface.java
@@ -16,11 +16,14 @@
 //
 package com.rs.game.content.skills.smithing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.rs.cache.loaders.ItemDefinitions;
-import com.rs.game.content.skills.smithing.Smithing.ForgingBar;
+import com.rs.cache.loaders.interfaces.IComponentDefinitions;
+import com.rs.game.content.skills.smithing.Smithing.Smithable;
 import com.rs.game.model.entity.player.Player;
 import com.rs.lib.Constants;
-import com.rs.lib.game.Item;
 import com.rs.lib.util.Utils;
 import com.rs.plugin.annotations.PluginEventHandler;
 import com.rs.plugin.events.ButtonClickEvent;
@@ -29,173 +32,127 @@ import com.rs.plugin.handlers.ButtonClickHandler;
 @PluginEventHandler
 public class ForgingInterface {
 
-	public static final int componentChilds[] = new int[30];
-	public static final int CLICKED_CHILDS[] = { 28, -1, 5, 1 };
 	private static final int SMITHING_INTERFACE = 300;
+	
+	public enum Slot {
+		DAGGER(18),
+		HATCHET(26),
+		MACE(34),
+		MEDIUM_HELM(42),
+		CROSSBOW_BOLTS(50),
+		SWORD(58),
+		DART_TIPS(66),
+		NAILS(74),
+		BRONZE_WIRE(82),
+		SPIT_IRON(90),
+		STUDS(98),
+		ARROW_TIPS(106),
+		SCIMITAR(114),
+		CROSSBOW_LIMBS(122),
+		LONGSWORD(130),
+		THROWING_KNIFE(138),
+		FULL_HELM(146),
+		SQUARE_SHIELD(154),
+		BULLSEYE_LANTERN(162),
+		GRAPPLE_TIP(170),
+		WARHAMMER(178),
+		BATTLEAXE(186),
+		CHAINBODY(194),
+		KITESHIELD(202),
+		CLAWS(210),
+		TWO_HAND_SWORD(218),
+		PLATESKIRT(226),
+		PLATELEGS(234),
+		PLATEBODY(242),
+		PICKAXE(267);
+		
+		private static final Map<Integer, Slot> CLICK_ID_MAP = new HashMap<>();
+		
+		static {
+			for (Slot s : Slot.values()) {
+				CLICK_ID_MAP.put(s.componentId+3, s); //make all
+				CLICK_ID_MAP.put(s.componentId+4, s); //make x
+				CLICK_ID_MAP.put(s.componentId+5, s); //make 5
+				CLICK_ID_MAP.put(s.componentId+6, s); //make 1
+			}
+		}
+		
+		public static Slot forId(int componentId) {
+			return CLICK_ID_MAP.get(componentId);
+		}
+		
+		private int componentId;
+		
+		Slot(int componentId) {
+			this.componentId = componentId;
+		}
+	}
 
-	public static ButtonClickHandler handleIComponents = new ButtonClickHandler(SMITHING_INTERFACE) {
+	public static ButtonClickHandler handleSmithButtons = new ButtonClickHandler(SMITHING_INTERFACE) {
 		@Override
 		public void handle(ButtonClickEvent e) {
-			int slot = -1;
-			int ticks = -1;
-			for (int i = 3; i <= 6; i++)
-				for (int index = 0; index < componentChilds.length; index++) {
-					if (componentChilds[index] + i != e.getComponentId())
-						continue;
-					slot = index;
-					ticks = CLICKED_CHILDS[i - 3];
-					break;
-				}
-			if (slot == -1)
+			int barId = e.getPlayer().getTempAttribs().getI("SmithingBar");
+			Slot slot = Slot.forId(e.getComponentId());
+			if (slot == null)
 				return;
-			e.getPlayer().getActionManager().setAction(new Smithing(ticks, slot));
+			Map<Slot, Smithable> items = Smithable.forBar(barId);
+			if (items == null || items.get(slot) == null)
+				return;
+			int makeX = switch(e.getComponentId() - slot.componentId) {
+			case 3 -> 28;
+			case 4 -> -1;
+			case 5 -> 5;
+			case 6 -> 1;
+			default -> 1;
+			};
+			if (makeX < 0)
+				e.getPlayer().sendInputInteger("How many would you like to make?", amount -> e.getPlayer().getActionManager().setAction(new Smithing(amount, items.get(slot))));
+			else
+				e.getPlayer().getActionManager().setAction(new Smithing(makeX, items.get(slot)));
 		}
 	};
 
-	private static void calculateComponentConfigurations() {
-		int base = 18;
-		for (int i = 0; i < componentChilds.length; i++) {
-			if (base == 250)
-				base = 267;
-			componentChilds[i] = base;
-			base += 8;
-		}
-	}
-
-	private static int getBasedAmount(Item item) {
-		String def = item.getDefinitions().getName();
-		if (def.contains("dagger"))
-			return 1;
-		if (def.contains("hatchet") || def.contains("mace") || def.contains("iron spit"))
-			return 2;
-		if (def.contains("bolts") || def.contains("med helm"))
-			return 3;
-		else if (def.contains("sword") || def.contains("dart tip") || def.contains("nails") || def.contains("wire"))
-			return 4;
-		else if (def.contains("arrow") || def.contains("pickaxe") || def.contains("scimitar"))
-			return 5;
-		else if (def.contains("longsword") || def.contains("limbs"))
-			return 6;
-		else if (def.contains("knife") || def.contains("full helm") || def.contains("studs"))
-			return 7;
-		else if (def.contains("sq shield") || def.contains("warhammer") || def.contains("grapple tip"))
-			return 9;
-		else if (def.contains("battleaxe"))
-			return 10;
-		else if (def.contains("chainbody") || def.contains("oil lantern"))
-			return 11;
-		else if (def.contains("kiteshield"))
-			return 12;
-		else if (def.contains("claws"))
-			return 13;
-		else if (def.contains("2h sword"))
-			return 14;
-		else if (def.contains("plateskirt") || def.contains("platelegs"))
-			return 16;
-		else if (def.contains("platebody"))
-			return 18;
-		else if (def.contains("bullseye lantern"))
-			return 19;
-		return 1;
-	}
-
-	public static int getFixedAmount(ForgingBar bar, Item item) {
-		String name = item.getDefinitions().getName();
-		int increment = getBasedAmount(item);
-		if (name.contains("dagger") && bar != ForgingBar.BRONZE)
-			increment--;
-		else if (name.contains("hatchet") && bar == ForgingBar.BRONZE)
-			increment--;
-		return increment;
-	}
-
-	public static int getForgedAmount(int id) {
-		String name = ItemDefinitions.getDefs(id).getName();
-		if (name.contains("knife"))
-			return 5;
-		if (name.contains("bolts") || name.contains("dart tip"))
-			return 10;
-		if (name.contains("arrowheads") || name.contains("nails"))
-			return 15;
-		return 1;
-	}
-
-	public static String[] getStrings(Player player, ForgingBar bar, int index, int itemId) {
-		if (itemId == -1 || index < 0 || index >= bar.getItems().length)
-			return null;
+	public static String[] getStrings(Player player, Smithable item) {
 		StringBuilder barName = new StringBuilder();
 		StringBuilder levelString = new StringBuilder();
-		String name = ItemDefinitions.getDefs(itemId).getName().toLowerCase();
-		String barVariableName = bar.toString().toLowerCase();
-		int levelRequired = bar.getLevel() + getFixedAmount(bar, bar.getItems()[index]);
-		int barAmount = getActualAmount(levelRequired, bar, itemId);
-		if (player.getInventory().getItems().getNumberOf(bar.getBarId()) >= barAmount)
+		String name = item.product.getDefinitions().getName().toLowerCase();
+		String barVariableName = item.toString().toLowerCase();
+		if (player.getInventory().getItems().getNumberOf(item.bar.getId()) >= item.bar.getAmount())
 			barName.append("<col=00FF00>");
-		barName.append(barAmount).append(" ").append(barAmount > 1 ? "bars" : "bar");
-		if (levelRequired >= 99)
-			levelRequired = 99;
-		if (player.getSkills().getLevel(Constants.SMITHING) >= levelRequired)
+		barName.append(item.bar.getAmount()).append(" ").append(item.bar.getAmount() > 1 ? "bars" : "bar");
+		if (player.getSkills().getLevel(Constants.SMITHING) >= item.level)
 			levelString.append("<col=FFFFFF>");
 		levelString.append(Utils.formatPlayerNameForDisplay(name.replace(barVariableName + " ", "")));
 		return new String[] { levelString.toString(), barName.toString() };
 	}
-
-	public static int getLevels(ForgingBar bar, int slot, Player player) {
-		int base = bar.getLevel();
-		int barAmount = getFixedAmount(bar, bar.getItems()[slot]);
-		int level = base + barAmount;
-		if (level > 99)
-			level = 99;
-		return level;
+	
+	public static void openSmithingInterfaceForHighestBar(Player player) {
+		int bar = Smithable.getHighestBar(player);
+		if (bar != -1)
+			sendSmithingInterface(player, bar);
+		else
+			player.sendMessage("You have no bars which you have smithing level to use.");
 	}
 
-	private static void sendComponentConfigs(Player player, ForgingBar bar) {
-		for (int i : bar.getComponentChilds())
-			player.getPackets().setIFHidden(SMITHING_INTERFACE, i - 1, false);
-	}
-
-	public static int getActualAmount(int levelRequired, ForgingBar bar, int id) {
-		if (levelRequired >= 99)
-			levelRequired = 99;
-		int level = levelRequired - bar.getLevel();
-		String name = ItemDefinitions.getDefs(id).getName().toLowerCase();
-		if (level >= 0 && level <= 4) {
-			if (name.contains("2h"))
-				return 3;
-			return 1;
-		}
-		if (level >= 4 && level <= 8) {
-			if (name.contains("knife") || name.contains("limb") || name.contains("studs") || name.contains("arrow"))
-				return 1;
-			return 2;
-		}
-		if (level >= 9 && level <= 16) {
-			if (name.contains("grapple"))
-				return 1;
-			else if (name.contains("claws"))
-				return 2;
-			return 3;
-		} else if (level >= 17) {
-			if (name.contains("bullseye"))
-				return 1;
-			return 5;
-		}
-		return 1;
-	}
-
-	public static void sendSmithingInterface(Player player, ForgingBar bar) {
-		calculateComponentConfigurations();
-		player.getTempAttribs().setO("SmithingBar", bar);
-		sendComponentConfigs(player, bar);
-		for (int i = 0; i < bar.getItems().length; i++) {
-			player.getPackets().setIFItem(SMITHING_INTERFACE, componentChilds[i], bar.getItems()[i].getId(), 1);
-			String[] name = getStrings(player, bar, i, bar.getItems()[i].getId());
+	public static void sendSmithingInterface(Player player, int barId) {
+		player.getTempAttribs().setI("SmithingBar", barId);
+		Map<Slot, Smithable> items = Smithable.forBar(barId);
+		for (Slot slot : Slot.values()) {
+			Smithable item = items.get(slot);
+			if (item == null && !IComponentDefinitions.getInterface(300)[slot.componentId].hidden) {
+				player.getPackets().setIFHidden(SMITHING_INTERFACE, slot.componentId-1, true);
+				continue;
+			}
+			if (IComponentDefinitions.getInterface(300)[item.slot.componentId-1].hidden)
+				player.getPackets().setIFHidden(SMITHING_INTERFACE, item.slot.componentId-1, false);
+			player.getPackets().setIFItem(SMITHING_INTERFACE, item.slot.componentId, item.product.getId(), item.product.getAmount());
+			String[] name = getStrings(player, item);
 			if (name != null) {
-				player.getPackets().setIFText(300, componentChilds[i] + 1, name[0]);
-				player.getPackets().setIFText(300, componentChilds[i] + 2, name[1]);
+				player.getPackets().setIFText(300, item.slot.componentId + 1, name[0]);
+				player.getPackets().setIFText(300, item.slot.componentId + 2, name[1]);
 			}
 		}
-		player.getPackets().setIFText(300, 14, Utils.formatPlayerNameForDisplay(bar.toString().toLowerCase()) + "");
+		player.getPackets().setIFText(300, 14, ItemDefinitions.getDefs(barId).name.replace(" bar", ""));
 		player.getInterfaceManager().sendInterface(SMITHING_INTERFACE);
 	}
 }
