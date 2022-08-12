9a64010cbd37ade7f4ce52cf94395e46a83e72b8
Trenton Kress <trenton.kress@gmail.com>
2022-08-12 11:35:32 -0700

    Merge more packet decoding additions

--- src/main/java/com/rs/net/decoders/handlers/impl/record/MouseClickHWHandler.java
+++ src/main/java/com/rs/net/decoders/handlers/impl/record/MouseClickHWHandler.java
@@ -2,13 +2,13 @@ package com.rs.net.decoders.handlers.impl.record;
 
 import com.rs.game.model.entity.player.Player;
 import com.rs.lib.net.packets.PacketHandler;
-import com.rs.lib.net.packets.decoders.mouse.MouseButtonClick;
+import com.rs.lib.net.packets.decoders.mouse.MouseClickHW;
 import com.rs.utils.record.impl.ClickHW;
 
-public class MouseButtonClickHandler implements PacketHandler<Player, MouseButtonClick> {
+public class MouseClickHWHandler implements PacketHandler<Player, MouseClickHW> {
 
 	@Override
-	public void handle(Player player, MouseButtonClick packet) {
+	public void handle(Player player, MouseClickHW packet) {
 		player.getRecorder().record(new ClickHW(packet.getTimeRecieved(), packet.getTime(), packet.getX(), packet.getY(), packet.getMouseButton(), packet.isHardware()));
 	}
 }
