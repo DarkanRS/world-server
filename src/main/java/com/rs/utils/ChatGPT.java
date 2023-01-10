3494b4119b75c22191685c6ca69fed317ccbff17
Trenton Kress <trenton.kress@gmail.com>
2023-01-09 16:46:55 -0800

    Update ChatGPT

--- src/main/java/com/rs/utils/ChatGPT.java
+++ src/main/java/com/rs/utils/ChatGPT.java
@@ -4,6 +4,9 @@ import com.rs.Settings;
 import com.theokanning.openai.OpenAiService;
 import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.completion.CompletionResult;
+import com.theokanning.openai.file.File;
+import com.theokanning.openai.finetune.FineTuneRequest;
+import com.theokanning.openai.finetune.FineTuneResult;
 
 public class ChatGPT {
 	private OpenAiService service;
@@ -14,22 +17,41 @@ public class ChatGPT {
 	
 	public String getResponse(String input) {
 		try {
-			CompletionRequest completionRequest = CompletionRequest.builder()
+			CompletionRequest req = CompletionRequest.builder()
 					.prompt(input)
 					.model("text-davinci-003")
-					.temperature(0.7)
-					.maxTokens(2048)
+					.temperature(0.0)
+					.maxTokens(300)
+					.topP(1.0)
+					.frequencyPenalty(0.0)
+					.presencePenalty(0.0)
 					.user("trenterinomemerino")
 					.build();
 	
-			CompletionResult choices = service.createCompletion(completionRequest);
-			if (choices.getChoices().isEmpty()) {
+			CompletionResult choices = service.createCompletion(req);
+			if (choices.getChoices().isEmpty())
 				return "No response available.";
-			}
 			return choices.getChoices().get(0).getText();
 		} catch(Throwable e) {
 			e.printStackTrace();
 			return "Error: " + e.getMessage();
+		}
+	}
+
+	public void fineTune(String jsonlPath) {
+		try {
+			File file = new File();
+			service.uploadFile("dialogueTraining", jsonlPath);
+			FineTuneRequest req = FineTuneRequest.builder()
+					.model("text-davinci-003")
+					.suffix("darkan-dialogue-create")
+					.trainingFile("dialogueTraining.jsonl")
+					.build();
+			
+			FineTuneResult result = service.createFineTune(req);
+			System.out.println(result.toString());
+		} catch(Throwable e) {
+			e.printStackTrace();
 		}
 	}
 }
