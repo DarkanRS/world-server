19f7f5b1734657e76537d8b272be5cc143f02394
Trenton Kress <trenton.kress@gmail.com>
2023-02-05 18:55:21 -0800

    Add kotlin loom coroutine bench

--- src/main/java/com/rs/utils/bench/KtLoomBench.kt
+++ src/main/java/com/rs/utils/bench/KtLoomBench.kt
@@ -3,29 +3,39 @@ package com.rs.utils.bench
 import com.rs.cache.Cache
 import com.rs.game.model.entity.pathing.FixedTileStrategy
 import com.rs.game.model.entity.pathing.RouteFinder
+import kotlinx.coroutines.ExecutorCoroutineDispatcher
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+private lateinit var executorService: ExecutorService
+private lateinit var executorCoroutineDispatcher: ExecutorCoroutineDispatcher
 
 fun main(args: Array<String>) {
+    executorService = Executors.newVirtualThreadPerTaskExecutor()
+    executorCoroutineDispatcher = executorService.asCoroutineDispatcher()
+
     for (i in 0..50) {
-        runCoroutineTest()
+        runLoomCoroutineTest()
     }
     println("JVM heated up. Starting...");
 
     var results: MutableList<Double> = mutableListOf();
     for (i in 0..50) {
         var start = System.nanoTime()
-        runCoroutineTest()
+        runLoomCoroutineTest()
         results.add((System.nanoTime() - start) / 1000000.0);
     }
 
     println("Calculated " + RouteFinder.COUNT + " routes across Burthorpe.")
-    println("Kotlin coroutines: "+results.average()+" ms")
+    println("Kotlin loom coroutines: "+results.average()+" ms")
 }
 
-fun runCoroutineTest() {
+fun runLoomCoroutineTest() {
     Cache.init("../cache/");
-    runBlocking {
+    runBlocking(executorCoroutineDispatcher) {
         (0 until 1000).map { i ->
             launch {
                 RouteFinder.find(2888, 3452, 0, 1, FixedTileStrategy(2917, 3524), true)
