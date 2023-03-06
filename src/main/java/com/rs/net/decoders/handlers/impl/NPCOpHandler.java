e18c6a2183ae3ca0c4cfdf105bbb2cd9b4b36718
Trenton Kress <trenton.kress@gmail.com>
2023-03-06 11:55:17 -0800

    Fix interaction height checks for chunk ids

--- src/main/java/com/rs/net/decoders/handlers/impl/NPCOpHandler.java
+++ src/main/java/com/rs/net/decoders/handlers/impl/NPCOpHandler.java
@@ -23,6 +23,7 @@ import com.rs.game.model.entity.player.Player;
 import com.rs.lib.net.ClientPacket;
 import com.rs.lib.net.packets.PacketHandler;
 import com.rs.lib.net.packets.decoders.NPCOp;
+import com.rs.lib.util.Utils;
 import com.rs.net.decoders.handlers.NPCHandler;
 
 public class NPCOpHandler implements PacketHandler<Player, NPCOp> {
@@ -36,8 +37,8 @@ public class NPCOpHandler implements PacketHandler<Player, NPCOp> {
 			player.setRun(true);
 		NPC npc = World.getNPCs().get(packet.getNpcIndex());
 
-		if (npc == null || npc.isDead() || npc.hasFinished() || !player.getMapChunkIds().contains(npc.getChunkId()) || npc.getDefinitions().getIdForPlayer(player.getVars()) == -1)
-			return;
+		if (npc == null || npc.isDead() || npc.hasFinished() || Utils.getDistanceI(player.getTile(), npc.getTile()) > 20 || npc.getDefinitions().getIdForPlayer(player.getVars()) == -1)
+		return;
 
 		if (packet.getOpcode() == ClientPacket.NPC_EXAMINE) {
 			NPCHandler.handleExamine(player, npc);
@@ -48,25 +49,25 @@ public class NPCOpHandler implements PacketHandler<Player, NPCOp> {
 
 		if (player.isDead() || player.isLocked())
 			return;
-		
+
 		switch(packet.getOpcode()) {
-		case NPC_OP1:
-			NPCHandler.handleOption1(player, npc);
-			break;
-		case NPC_OP2:
-			NPCHandler.handleOption2(player, npc);
-			break;
-		case NPC_OP3:
-			NPCHandler.handleOption3(player, npc);
-			break;
-		case NPC_OP4:
-			NPCHandler.handleOption4(player, npc);
-			break;
-		case NPC_OP5:
-			NPCHandler.handleOption5(player, npc);
-			break;
-		default:
-			break;
+			case NPC_OP1:
+				NPCHandler.handleOption1(player, npc);
+				break;
+			case NPC_OP2:
+				NPCHandler.handleOption2(player, npc);
+				break;
+			case NPC_OP3:
+				NPCHandler.handleOption3(player, npc);
+				break;
+			case NPC_OP4:
+				NPCHandler.handleOption4(player, npc);
+				break;
+			case NPC_OP5:
+				NPCHandler.handleOption5(player, npc);
+				break;
+			default:
+				break;
 		}
 	}
 
