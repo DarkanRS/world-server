f3c5dd5b95304657ade3ddab4b6bdce889d07df1
Brandon Fischer <bfischercs@gmail.com>
2023-07-02 21:46:42 -0400

    Removed "added" inventories not from cache. Added spawns/fixed spawns in slayer dungeon as well as portals.

--- src/main/java/com/rs/game/content/world/areas/pollnivneach/PollnivneachSlayerDungeon.java
+++ src/main/java/com/rs/game/content/world/areas/pollnivneach/PollnivneachSlayerDungeon.java
@@ -63,4 +63,19 @@ public class PollnivneachSlayerDungeon {
 	public static ObjectClickHandler handleUpStairsNW = new ObjectClickHandler(new Object[] { 31417 }, new Tile[] { Tile.of(3277, 4367, 0) }, e -> {
 		e.getPlayer().useStairs(e.getPlayer().transform(68, 5059));
 	});
+
+	public static ObjectClickHandler handlepulsingmarkingkurask = new ObjectClickHandler(new Object[] { 31363 }, Tile.of(3295, 4407, 0), e -> {
+		e.getPlayer().setNextTile(Tile.of(2700, 9999, 0));
+	});
+
+	public static ObjectClickHandler handlepulsingmarkingcavecrawler = new ObjectClickHandler(new Object[] { 31363 }, Tile.of(3325, 4391, 0), e -> {
+		e.getPlayer().setNextTile(Tile.of(2789, 9997, 0));
+	});
+
+	public static ObjectClickHandler handlepulsingmarkingbasilisk = new ObjectClickHandler(new Object[] { 31363 }, Tile.of(3294, 4293, 0), e -> {
+		e.getPlayer().setNextTile(Tile.of(2743, 10009, 0));
+	});
+	public static ObjectClickHandler handlepulsingmarkingturoth = new ObjectClickHandler(new Object[] { 31363 }, Tile.of(3288, 4293, 0), e -> {
+		e.getPlayer().setNextTile(Tile.of(2723, 10005, 0));
+	});
 }
