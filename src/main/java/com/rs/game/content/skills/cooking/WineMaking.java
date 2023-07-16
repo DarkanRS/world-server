d55560ee53d7965ec837c0014883cf3094dc9c8b
tilkinsc <ctilkins@mail.com>
2023-07-15 20:24:05 -0400

    Add winemaking

--- src/main/java/com/rs/game/content/skills/cooking/WineMaking.java
+++ src/main/java/com/rs/game/content/skills/cooking/WineMaking.java
@@ -2,25 +2,32 @@ package com.rs.game.content.skills.cooking;
 
 import com.rs.game.model.entity.player.Player;
 import com.rs.game.model.entity.player.actions.PlayerAction;
+import com.rs.game.tasks.WorldTasks;
 import com.rs.lib.Constants;
-import com.rs.plugin.annotations.PluginEventHandler;
-import com.rs.plugin.handlers.ItemOnItemHandler;
+import com.rs.lib.game.Animation;
+import com.rs.plugin.handlers.LoginHandler;
 
-@PluginEventHandler
-public class Wine extends PlayerAction {
+public class WineMaking extends PlayerAction {
 
-	private boolean rollBadWine(Player player)
-	{
+	private boolean rollBadWine(Player player) {
 		double level = player.getSkills().getLevel(Constants.COOKING);
 		double noFailLevel = 68.0D;
 		double chance = Math.max(level / noFailLevel, 0.6D);
 		return chance < Math.random();
 	}
 
+	private final int JUG_OF_WATER = 1;
+	private final int GRAPES = 1;
+	private final int UNFERMENTED_WINE = 1;
+	private final int JUG_OF_WINE = 1;
+
 	private int amount;
 
-	public Wine(int amount)
-	{
+	public static LoginHandler onLogin = new LoginHandler(e -> {
+
+	});
+
+	public WineMaking(int amount) {
 		this.amount = amount;
 	}
 
@@ -39,19 +46,27 @@ public class Wine extends PlayerAction {
 			player.simpleDialogue("You need a cooking level of 35 to make wine.");
 			return false;
 		}
-
 		return true;
 	}
+
+	private final int WINE_CREATE_TICKS = 2;
 
 	@Override
 	public int processWithDelay(Player player) {
 		amount--;
-		
-		return 0;
+		player.setNextAnimation(new Animation(1));
+		player.getInventory().deleteItem(GRAPES, 1);
+		player.getInventory().deleteItem(JUG_OF_WATER, 1);
+		player.getInventory().addItem(UNFERMENTED_WINE);
+		player.setWineCreated();
+		return amount > 0 ? WINE_CREATE_TICKS : -1;
 	}
 
 	@Override
-	public void stop(Player entity) {
+	public void stop(Player player) {
+		WorldTasks.schedule(0, 1, () -> {
 
+		});
 	}
+
 }
