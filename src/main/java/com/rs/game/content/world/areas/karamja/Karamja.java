3a94ced72a165b843a8875eb1f95744145f793af
tilkinsc <ctilkins@mail.com>
2023-11-06 18:38:52 -0500

    Added Gabooty Shop

--- src/main/java/com/rs/game/content/world/areas/karamja/Karamja.java
+++ src/main/java/com/rs/game/content/world/areas/karamja/Karamja.java
@@ -41,6 +41,16 @@ import com.rs.utils.shop.ShopsHandler;
 @PluginEventHandler
 public class Karamja  {
 
+	public static NPCClickHandler handleGabooty = new NPCClickHandler(new Object[] { 2520 }, e -> {
+		if (!e.getPlayer().isQuestComplete(Quest.JUNGLE_POTION))
+			return;
+		switch (e.getOption())
+		{
+			case "Trade-Co-op" -> ShopsHandler.openShop(e.getPlayer(), "gabootys_tai_bwo_wannai_cooperative");
+			case "Trade-Drinks" -> ShopsHandler.openShop(e.getPlayer(), "gabootys_tai_bwo_wannai_drinky_store");
+		}
+	});
+
 	public static NPCClickHandler handlePirateJackieFruit = new NPCClickHandler(new Object[] { 1055 }, e -> {
 		e.getPlayer().startConversation(new Conversation(e.getPlayer()) {
 			{
