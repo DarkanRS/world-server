568f05c2ada6bd04a1a23618ddeef65ca74a2597
Trenton Kress <trenton.kress@gmail.com>
2023-11-08 16:04:02 -0800

    Fix up some switch statements in Darren's dialogues

--- src/main/java/com/rs/game/content/quests/buyersandcellars/npcs/DarrenLightfingerI.java
+++ src/main/java/com/rs/game/content/quests/buyersandcellars/npcs/DarrenLightfingerI.java
@@ -9,26 +9,20 @@ import com.rs.game.model.object.GameObject;
 import com.rs.lib.game.Tile;
 import com.rs.plugin.annotations.PluginEventHandler;
 import com.rs.plugin.handlers.NPCClickHandler;
+
 @PluginEventHandler
 public class DarrenLightfingerI extends Dialogue {
     private static final int npcid = 11273;
-    public static NPCClickHandler DarrenLightfingerI  = new NPCClickHandler(new Object[] { npcid }, new String[] {"Talk-to"}, e -> {
-        if (!e.getPlayer().isQuestStarted(Quest.BUYERS_AND_CELLARS)){
+    public static NPCClickHandler DarrenLightfingerI = new NPCClickHandler(new Object[]{npcid}, new String[]{"Talk-to"}, e -> {
+        if (!e.getPlayer().isQuestStarted(Quest.BUYERS_AND_CELLARS)) {
             preQuest(e.getPlayer());
             return;
         }
-        if (e.getPlayer().getQuestStage(Quest.BUYERS_AND_CELLARS) == 1){
-            preQuest(e.getPlayer());
-            return;
+        switch (e.getPlayer().getQuestStage(Quest.BUYERS_AND_CELLARS)) {
+            case 1 -> preQuest(e.getPlayer());
+            case 2 -> stage2(e.getPlayer());
+            default -> stage3(e.getPlayer());
         }
-        if (e.getPlayer().getQuestStage(Quest.BUYERS_AND_CELLARS) == 2) {
-            stage2(e.getPlayer());
-            return;
-        }
-        if (e.getPlayer().getQuestStage(Quest.BUYERS_AND_CELLARS) >= 3) {
-            stage3(e.getPlayer());
-        }
-
     });
 
     public static void stage3(Player player) {
@@ -38,8 +32,8 @@ public class DarrenLightfingerI extends Dialogue {
                     ops.add("Can we try out that testing dummy again?")
                             .addPlayer(HeadE.CALM_TALK, "Can we try out that testing dummy again?")
                             .addNext(() -> {
-                                player.npcDialogue(npcid,HeadE.HAPPY_TALKING,"Of course!");
-                                player.walkToAndExecute(Tile.of(4664,5903,0), () -> player.getActionManager().setAction(new PickPocketDummy(new GameObject(52316, 1, 4665, 5903, 0))));
+                                player.npcDialogue(npcid, HeadE.HAPPY_TALKING, "Of course!");
+                                player.walkToAndExecute(Tile.of(4664, 5903, 0), () -> player.getActionManager().setAction(new PickPocketDummy(new GameObject(52316, 1, 4665, 5903, 0))));
                             });
 
 
@@ -53,28 +47,20 @@ public class DarrenLightfingerI extends Dialogue {
                             .addNPC(npcid, HeadE.HAPPY_TALKING, "Have you retrieved the chalice?")
                             .addNext(() -> {
                                 switch (player.getQuestStage(Quest.BUYERS_AND_CELLARS)) {
-                                    case 3 -> {
-                                        player.startConversation(new Dialogue()
-                                                .addPlayer(HeadE.CALM_TALK, "Not yet.")
-                                                .addNPC(npcid, HeadE.HAPPY_TALKING, "Head to Lumbridge Castle as soon as you may, then; Robin will meet you there.")
-                                        );
-                                    }
-                                    case 4 -> {
-                                        player.startConversation(new Dialogue()
-                                                .addPlayer(HeadE.CALM_TALK, "Not yet, but I'm on its trail")
-                                        );
-                                    }
-                                    case 5, 6 -> {
-                                        player.startConversation(new Dialogue()
-                                                .addPlayer(HeadE.CALM_TALK, "I've tracked it down, but I've not yet retrieved it.")
-                                        );
-                                    }
-                                    case 7 -> {
-                                        player.startConversation(new Dialogue()
-                                                .addPlayer(HeadE.CALM_TALK, "I have the key, but not the chalice.")
-                                                .addNPC(npcid, HeadE.CALM_TALK, "You've used several keys in the past, I'm sure; one more should pose no difficulty.")
-                                        );
-                                    }
+                                    case 3 -> player.startConversation(new Dialogue()
+                                            .addPlayer(HeadE.CALM_TALK, "Not yet.")
+                                            .addNPC(npcid, HeadE.HAPPY_TALKING, "Head to Lumbridge Castle as soon as you may, then; Robin will meet you there."));
+
+                                    case 4 -> player.startConversation(new Dialogue()
+                                            .addPlayer(HeadE.CALM_TALK, "Not yet, but I'm on its trail"));
+
+                                    case 5, 6 -> player.startConversation(new Dialogue()
+                                            .addPlayer(HeadE.CALM_TALK, "I've tracked it down, but I've not yet retrieved it."));
+
+                                    case 7 -> player.startConversation(new Dialogue()
+                                            .addPlayer(HeadE.CALM_TALK, "I have the key, but not the chalice.")
+                                            .addNPC(npcid, HeadE.CALM_TALK, "You've used several keys in the past, I'm sure; one more should pose no difficulty.")
+                                    );
                                     case 8 -> player.startConversation(new Dialogue()
                                             .addPlayer(HeadE.HAPPY_TALKING, "I have!")
                                             .addNPC(npcid, HeadE.HAPPY_TALKING, "Fantastic work! I knew I had chosen wisely when I recruited you. Now we can expand the guild and do some proper training around here.")
@@ -90,8 +76,7 @@ public class DarrenLightfingerI extends Dialogue {
                                                 player.getVars().saveVarBit(7792, 10);
                                                 player.getVars().setVarBit(7793, 0);
                                                 player.getQuestManager().completeQuest(Quest.BUYERS_AND_CELLARS);
-                                            }))
-                                    );
+                                            })));
                                 }
                             });
                     ops.add("Sorry, I was just leaving.")
@@ -147,7 +132,7 @@ public class DarrenLightfingerI extends Dialogue {
                             .addNPC(npcid, HeadE.HAPPY_TALKING, "Here you will become the best of the best, joining the ranks of the steely-eyed exploiters of this world's bloated social parasites in a quest for community justice and personal enrichment!")
                             .addNPC(npcid, HeadE.HAPPY_TALKING, "You will - Yes, we have a question?")
                             .addPlayer(HeadE.SKEPTICAL_THINKING, "Is this it? The 'world-renowned' guild is a cellar with two blokes and a straw dummy lurking in it?")
-                            .addNPC(npcid,HeadE.CONFUSED, "Well...")
+                            .addNPC(npcid, HeadE.CONFUSED, "Well...")
                             .addPlayer(HeadE.CONFUSED, "How long has this guild been in operation?")
                             .addNPC(npcid, HeadE.CALM, "...")
                             .addPlayer(HeadE.CONFUSED, "Yes?")
@@ -180,7 +165,7 @@ public class DarrenLightfingerI extends Dialogue {
                                         .addNPC(npcid, HeadE.HAPPY_TALKING, "Here you will become the best of the best, joining the ranks of the steely-eyed exploiters of this world's bloated social parasites in a quest for community justice and personal enrichment!")
                                         .addNPC(npcid, HeadE.HAPPY_TALKING, "You will - Yes, we have a question?")
                                         .addPlayer(HeadE.SKEPTICAL_THINKING, "Is this it? The 'world-renowned' guild is a cellar with two blokes and a straw dummy lurking in it?")
-                                        .addNPC(npcid,HeadE.CONFUSED, "Well...")
+                                        .addNPC(npcid, HeadE.CONFUSED, "Well...")
                                         .addPlayer(HeadE.CONFUSED, "How long has this guild been in operation?")
                                         .addNPC(npcid, HeadE.CALM, "...")
                                         .addPlayer(HeadE.CONFUSED, "Yes?")
@@ -194,21 +179,21 @@ public class DarrenLightfingerI extends Dialogue {
                                         .addPlayer(HeadE.SKEPTICAL, "I take it you have some sort of plan for doing this?")
                                         .addNPC(npcid, HeadE.HAPPY_TALKING, "Of course I do. In Lumbridge Castle's bank there is a golden chalice of particular workmanship and value. I have found a willing buyer for it, and now it merely remains to collect the item in question.")
                                         .addPlayer(HeadE.SKEPTICAL, "From the bank's vault..")
-                                        .addNPC(npcid,HeadE.HAPPY_TALKING, "Correct! And since the vault is not easily breached, we shall need the key from it's owner.")
+                                        .addNPC(npcid, HeadE.HAPPY_TALKING, "Correct! And since the vault is not easily breached, we shall need the key from it's owner.")
                                         .addPlayer(HeadE.CALM_TALK, "It's owner being?")
                                         .addNPC(npcid, HeadE.HAPPY_TALKING, "I shall send my right hand man, Robin, to determine that as soon as I may. He shall be around to brief you in the castle ground.")
                                         .addNPC(npcid, HeadE.HAPPY_TALKING, "Then you must merely acquire the key by stealth or by force, open the vault, return the chalice and...")
                                         .addPlayer(HeadE.SKEPTICAL_THINKING, "You seem to be assuming a certain amount here.")
                                         .addNPC(npcid, HeadE.HAPPY_TALKING, "Oh, but of course you'll help! I can offer you the best of training and the greatest of rewards for your assistance.. In fact, Let me have a look at your technique and see what we can do with you.")
-                                        .addOptions( ops3 -> {
+                                        .addOptions(ops3 -> {
                                             ops3.add("Accept quest")
-                                                    .addPlayer(HeadE.CALM_TALK, "Oh very well!",() -> player.setQuestStage(Quest.BUYERS_AND_CELLARS, 1))
+                                                    .addPlayer(HeadE.CALM_TALK, "Oh very well!", () -> player.setQuestStage(Quest.BUYERS_AND_CELLARS, 1))
                                                     .addNPC(npcid, HeadE.HAPPY_TALKING, "Splendid! Let's get you set up then.")
                                                     .addNPC(npcid, HeadE.HAPPY_TALKING, "This is a Mark 1 training dummy.")
-                                                    .addNPC(npcid, HeadE.CALM_TALK,"It's designed for maximum pocket size and minimum observation skills, which, seeing as it's made of wood, straw, and canvas, was not hard to achieve.")
+                                                    .addNPC(npcid, HeadE.CALM_TALK, "It's designed for maximum pocket size and minimum observation skills, which, seeing as it's made of wood, straw, and canvas, was not hard to achieve.")
                                                     .addNPC(npcid, HeadE.CALM_TALK, "It will suffice for early training and for testing, but if you have any talent at all it will not be of use to you for long.")
                                                     .addNPC(npcid, HeadE.CALM, "Right, I want you to pick the pocket of that dummy as sneakily and delicately as you possibly can.")
-                                                    .addNext(() -> player.walkToAndExecute(Tile.of(4664,5903,0), () -> {
+                                                    .addNext(() -> player.walkToAndExecute(Tile.of(4664, 5903, 0), () -> {
                                                         player.getActionManager().setAction(new PickPocketDummy(new GameObject(52316, 1, 4665, 5903, 0)));
                                                         player.lock();
                                                     }));
