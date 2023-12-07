712f85f744dc2f3aa209f1ec03deb1d92944b918
Trenton Kress <trenton.kress@gmail.com>
2023-12-06 16:43:43 -0800

    Add animations to avatars for soul wars. Continue on implementing the structures and data for teams, lobby, and ingame UI updating in soul wars

--- src/main/java/com/rs/game/content/minigames/soulwars/SoulWars.kt
+++ src/main/java/com/rs/game/content/minigames/soulwars/SoulWars.kt
@@ -1,42 +1,53 @@
 package com.rs.game.content.minigames.soulwars
 
+import com.rs.game.model.entity.npc.NPC
 import com.rs.game.model.entity.player.Controller
+import com.rs.game.model.entity.player.Player
+import com.rs.game.tasks.WorldTasks
 import com.rs.lib.game.Tile
 import com.rs.plugin.annotations.ServerStartupEvent
+import com.rs.plugin.kts.instantiateNpc
 import com.rs.plugin.kts.onButtonClick
 import com.rs.plugin.kts.onNpcClick
 import com.rs.plugin.kts.onObjectClick
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet
+import it.unimi.dsi.fastutil.objects.ObjectSet
+import it.unimi.dsi.fastutil.objects.ObjectSets
+import kotlinx.coroutines.internal.synchronized
 
-val LOBBY_OVERLAY = 837
-val INGAME_OVERLAY = 836
+const val LOBBY_OVERLAY = 837
+const val INGAME_OVERLAY = 836
 
-val GAME_ACTIVE_VARC = 632
-val BLUE_TEAM_SIZE_VARC = 633
-val RED_TEAM_SIZE_VARC = 634
+const val GAME_ACTIVE_VARC = 632
+const val BLUE_TEAM_SIZE_VARC = 633
+const val RED_TEAM_SIZE_VARC = 634
 
-val LOBBY_MINUTES_PASSED_VARC = 635
-val GAME_MINUTES_PASSED_VARC = 636
+const val LOBBY_MINUTES_PASSED_VARC = 635
+const val GAME_MINUTES_PASSED_VARC = 636
 
-val BLUE_AVATAR_HEALTH_VARC = 639
-val RED_AVATAR_HEALTH_VARC = 640
-val BLUE_AVATAR_LEVEL_VARC = 641
-val RED_AVATAR_LEVEL_VARC = 642
-val BLUE_AVATAR_DEATH_VARC = 643
-val RED_AVATAR_DEATH_VARC = 644
+const val BLUE_AVATAR_HEALTH_VARC = 639
+const val RED_AVATAR_HEALTH_VARC = 640
+const val BLUE_AVATAR_LEVEL_VARC = 641
+const val RED_AVATAR_LEVEL_VARC = 642
+const val BLUE_AVATAR_DEATH_VARC = 643
+const val RED_AVATAR_DEATH_VARC = 644
 
-val MID_CLAIM_VARC = 645 //0-30  0 being blue owned 30 being red owned
-val MID_CLAIM_BAR_COMP = 47
-val EAST_CLAIM_VARC = 647 //0-30  0 being blue owned 30 being red owned
-val EAST_CLAIM_BAR_COMP = 49
-val WEST_CLAIM_VARC = 649 //0-30  0 being blue owned 30 being red owned
-val WEST_CLAIM_BAR_COMP = 50
+const val MID_CLAIM_VARC = 645 //0-30  0 being blue owned 30 being red owned
+const val MID_CLAIM_BAR_COMP = 47
+const val EAST_CLAIM_VARC = 647 //0-30  0 being blue owned 30 being red owned
+const val EAST_CLAIM_BAR_COMP = 49
+const val WEST_CLAIM_VARC = 649 //0-30  0 being blue owned 30 being red owned
+const val WEST_CLAIM_BAR_COMP = 50
 
-val PLAYER_ACTIVITY_BAR_VAR = 1380 //0-1000
+const val PLAYER_ACTIVITY_BAR_VAR = 1380 //0-1000
 
-val REFRESH_RED_TEAM_COUNT_SCRIPT = 2075
-val REFRESH_BLUE_TEAM_COUNT_SCRIPT = 2076
-val REFRESH_TIME = 2077
+const val REFRESH_RED_TEAM_COUNT_SCRIPT = 2075
+const val REFRESH_BLUE_TEAM_COUNT_SCRIPT = 2076
+const val REFRESH_TIME = 2077
 
+val LOBBY_PLAYERS: ObjectSet<Player> = ObjectSets.synchronize(ObjectOpenHashSet())
+val INGAME_PLAYERS: ObjectSet<Player> = ObjectSets.synchronize(ObjectOpenHashSet())
+val ACTIVE_GAME = SoulWars()
 
 @ServerStartupEvent
 fun mapHandlers() {
@@ -61,23 +72,37 @@ fun mapHandlers() {
     }
 }
 
-class SoulWars {
+class SoulAvatar(redTeam: Boolean) : NPC(if (redTeam) 8596 else 8597, if (redTeam) Tile.of(1965, 3249, 0) else Tile.of(1805, 3208, 0)) {
+    var level = 99
 
+}
+
+class SoulWars {
+    val redTeam: ObjectSet<Player> = ObjectSets.synchronize(ObjectOpenHashSet())
+    val blueTeam: ObjectSet<Player> = ObjectSets.synchronize(ObjectOpenHashSet())
+    var redAvatar = SoulAvatar(true)
+    var blueAvatar = SoulAvatar(false)
 }
 
 class SoulWarsLobbyController : Controller() {
     override fun start() {
         player.interfaceManager.sendOverlay(LOBBY_OVERLAY)
+        LOBBY_PLAYERS.add(player)
     }
 
     override fun login(): Boolean {
         player.interfaceManager.sendOverlay(LOBBY_OVERLAY)
+        LOBBY_PLAYERS.add(player)
         return false
     }
 
-    override fun logout(): Boolean { return false }
+    override fun logout(): Boolean {
+        LOBBY_PLAYERS.remove(player)
+        return false
+    }
 
     override fun onRemove() {
+        LOBBY_PLAYERS.remove(player)
         player.interfaceManager.removeOverlay()
     }
 }
@@ -85,16 +110,22 @@ class SoulWarsLobbyController : Controller() {
 class SoulWarsGameController : Controller() {
     override fun start() {
         player.interfaceManager.sendOverlay(INGAME_OVERLAY)
+        INGAME_PLAYERS.add(player)
     }
 
     override fun login(): Boolean {
         player.interfaceManager.sendOverlay(INGAME_OVERLAY)
+        INGAME_PLAYERS.add(player)
         return false
     }
 
-    override fun logout(): Boolean { return false }
+    override fun logout(): Boolean {
+        INGAME_PLAYERS.remove(player)
+        return false
+    }
 
     override fun onRemove() {
+        INGAME_PLAYERS.remove(player)
         player.interfaceManager.removeOverlay()
     }
 }
