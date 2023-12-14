1e7e7fec6cedad72ba64b1011ae251327da1409e
Trenton Kress <trenton.kress@gmail.com>
2023-12-14 12:32:18 -0800

    Add utility function for grabbing item containers off metadata TreeMaps

---
+++ src/main/java/com/rs/game/content/TrophyBones.kt
@@ -0,0 +1,27 @@
+package com.rs.game.content;
+
+import com.rs.game.content.items.LootInterface
+import com.rs.plugin.annotations.ServerStartupEvent
+import com.rs.plugin.kts.onItemOnObject
+import com.rs.utils.WorldUtil.gsonTreeMapToItemContainer
+
+@ServerStartupEvent
+fun mapTrophyBones() {
+    onItemOnObject(arrayOf("Bank booth", "Bank", "Bank chest", "Bank table", "Counter", "Shantay chest", "Darkmeyer Treasury"), arrayOf(24444)) { e ->
+        e.apply {
+            if (!getObject().getDefinitions(player).containsOption("Bank") && !getObject().getDefinitions(player).containsOption("Use")) {
+                player.sendMessage("This isn't a proper bank.")
+                return@onItemOnObject
+            }
+            player.inventory.items.set(item.slot, null)
+            player.inventory.refresh(item.slot)
+
+            val items = gsonTreeMapToItemContainer(item.getMetaDataO("trophyBoneItems"))
+            if (items == null) {
+                player.sendMessage("Your trophy bones don't contain anything.")
+                return@onItemOnObject
+            }
+            LootInterface.open((item.getMetaDataO("trophyBoneOriginator") ?: "Unknown") + "'s Corpse", e.player, items)
+        }
+    }
+}
