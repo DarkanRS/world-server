32d2747e1f62cc1de7eda137b669e6daa9e5ee85
Trenton Kress <trenton.kress@gmail.com>
2024-01-05 15:59:36 -0800

    Update voice sound effect dump to be way more accurate. Thanks @MKP

--- src/main/java/com/rs/game/content/clans/clan_camp/npcs/CaptainOfTheGuard.java
+++ src/main/java/com/rs/game/content/clans/clan_camp/npcs/CaptainOfTheGuard.java
@@ -1,4 +1,4 @@
-package com.rs.game.content.clans.clanCamp.npcs;
+package com.rs.game.content.clans.clan_camp.npcs;
 
 import com.rs.engine.dialogue.Conversation;
 import com.rs.engine.dialogue.Dialogue;
@@ -7,28 +7,24 @@ import com.rs.game.model.entity.player.Player;
 import com.rs.plugin.annotations.PluginEventHandler;
 import com.rs.plugin.handlers.NPCClickHandler;
 
-import static com.rs.game.content.clans.ClansManager.promptName;
-
 @PluginEventHandler
-public class Scribe extends Conversation {
+public class CaptainOfTheGuard extends Conversation {
 
-    private static final int npcId = 5915;
+    private static final int npcId = 13633;
 
     private boolean hasClan;
 
-    private int Vexilum = 20709;
-
     private String title = player.getAppearance().isMale() ? "sir" : "ma'am";
 
-    public static NPCClickHandler Scribe = new NPCClickHandler(new Object[] { npcId }, e -> {
+    public static NPCClickHandler CaptainOfTheGuard = new NPCClickHandler(new Object[] { npcId }, e -> {
         String t = e.getPlayer().getAppearance().isMale() ? "sir" : "ma'am";
         if (e.getOption().equalsIgnoreCase("talk-to")) {
-            e.getPlayer().startConversation(new Scribe(e.getPlayer()));
+            e.getPlayer().startConversation(new CaptainOfTheGuard(e.getPlayer()));
         }
-        if (e.getOption().equalsIgnoreCase("get-vexillum")) {
+        if (e.getOption().equalsIgnoreCase("get-cloak")) {
             if(e.getPlayer().getClan() == null){
                 e.getPlayer().startConversation(new Dialogue()
-                        .addNPC(npcId, HeadE.HAPPY_TALKING, "You must be in a clan to have a vexillum, " + t + ".")
+                        .addNPC(npcId, HeadE.HAPPY_TALKING, "You must be in a clan to have a clan cloak, " + t + ".")
                 );
                 return;
             }
@@ -38,42 +34,31 @@ public class Scribe extends Conversation {
                 );
                 return;
             }
-            if(e.getPlayer().getInventory().hasFreeSlots() && e.getPlayer().getInventory().containsItem(20709)) {
+            if(e.getPlayer().getInventory().hasFreeSlots() && e.getPlayer().getInventory().containsItem(20708)) {
                 e.getPlayer().startConversation(new Dialogue()
-                        .addNPC(npcId, HeadE.HAPPY_TALKING, "You can only have one clan vexillum, " + t + ". Those are the rules")
+                        .addNPC(npcId, HeadE.HAPPY_TALKING, "You can only have one clan cloak, " + t + ". Those are the rules")
                 );
                 return;
             }
             else {
                 e.getPlayer().startConversation(new Dialogue()
-                        .addNPC(npcId, HeadE.HAPPY_TALKING, "Certainly, " + t + ". Here you are..")
+                        .addNPC(npcId, HeadE.HAPPY_TALKING, "Certainly. Here you are..")
                 );
-                e.getPlayer().getInventory().addItem(20709);
-                e.getPlayer().sendMessage("The scribe gives you a vexillum.");
+                e.getPlayer().getInventory().addItem(20708);
+                e.getPlayer().sendMessage("The captain gives you a cloak.");
             }
         }
     });
 
 
-
-
-    public Scribe(Player player) {
+    public CaptainOfTheGuard(Player player) {
         super(player);
         if(player.getClan() != null)
             hasClan = true;
         if(!hasClan)
             player.startConversation(new Dialogue()
-                    .addNPC(npcId, HeadE.HAPPY_TALKING, "Clan scribe Amos Twinly at your service, " + title +". I keep a record of all clans, and distribute clan charters to those who wish to start a clan. ")
-                    .addNPC(npcId, HeadE.HAPPY_TALKING,  "If you are a member of a clan, I can also provide you with a vexillum - a banner displaying your clan's motif.")
-
-                    .addOptions(clanOps -> {
-                        clanOps.add("I'd like to charter a new clan")
-                                .addNPC(npcId, HeadE.HAPPY_TALKING, "Splendid news " + title + ". What would you like your clan to be called?")
-                                .addNext(() -> promptName(player));
-
-                        clanOps.add("Goodbye")
-                                .addNPC(npcId, HeadE.HAPPY_TALKING, "Goodbye");
-                    }));
+                    .addNPC(npcId, HeadE.HAPPY_TALKING, "Alf A Numerius at your service, " + title +". It's my job to give out clan cloaks. If you'd like to know more about clans, talk to the clan scribe.")
+            );
         else
             player.startConversation(new Dialogue()
                     .addNPC(npcId, HeadE.HAPPY_TALKING, "Need something, " + title + "?")
@@ -96,7 +81,7 @@ public class Scribe extends Conversation {
                                                 .addNPC(npcId, HeadE.HAPPY_TALKING, "Certainly. Here you are " + title + ".")
                                         );
                                         player.getInventory().addItem(20708);
-                                        player.sendMessage("The scribe gives you a clan cloak.");
+                                        player.sendMessage("The captain gives you a cloak.");
                                     }
 
                                 });
