6f533c9426238892f243c79719996a3ff45a6d93
Devin DuBose <devin.a.dubose@gmail.com>
2024-07-26 16:37:23 +0000

    Fix slayer helm check

--- src/main/java/com/rs/game/model/entity/player/Equipment.java
+++ src/main/java/com/rs/game/model/entity/player/Equipment.java
@@ -41,6 +41,7 @@ import com.rs.plugin.handlers.ButtonClickHandler;
 import com.rs.plugin.handlers.InterfaceOnInterfaceHandler;
 import com.rs.utils.ItemConfig;
 
+import java.util.Arrays;
 import java.util.List;
 
 @PluginEventHandler
@@ -463,8 +464,9 @@ public final class Equipment {
 	}
 
 	public boolean wearingBlackMask() {
+		int[] masks = new int[] { 8921, 8919, 8917, 8915, 8913, 8911, 8909, 8907, 8905, 8903, 8901 };
 		if (items.get(HEAD) != null)
-            return ItemDefinitions.getDefs(items.get(HEAD).getId()).getName().toLowerCase().contains("black mask");
+			return Arrays.stream(masks).anyMatch(i -> i == items.get(HEAD).getId());
 		return false;
 	}
 
@@ -481,8 +483,9 @@ public final class Equipment {
 	}
 
 	public boolean wearingSlayerHelmet() {
+		int[] helms = new int[] { 13263, 14636, 14637, 15492, 15496, 15497, 22528, 22529, 22530, 22531, 22532, 22533, 22534, 22535, 22536, 22537, 22538, 22539, 22540, 225541, 22542, 22543, 22544, 22545, 22546, 22547, 22548, 22549, 22550, 22551 };
 		if (items.get(HEAD) != null)
-            return items.get(HEAD).getId() == 15492 || items.get(HEAD).getId() == 15496 || items.get(HEAD).getId() == 15497;
+			return Arrays.stream(helms).anyMatch(i -> i == items.get(HEAD).getId());
 		return false;
 	}
 
