1de738b85ddc2fde5817a10c554b0aff5efdf404
Trenton Kress <trenton.kress@gmail.com>
2024-08-25 05:04:51 -0700

    Fix some lost her marbles formatting

--- src/main/java/com/rs/game/content/miniquests/lost_her_marbles/LostHerMarbles.kt
+++ src/main/java/com/rs/game/content/miniquests/lost_her_marbles/LostHerMarbles.kt
@@ -7,10 +7,6 @@ import com.rs.game.model.entity.player.Player
 import com.rs.game.model.entity.player.Skills
 import com.rs.plugin.annotations.ServerStartupEvent
 
-@ServerStartupEvent
-fun mapLostHerMarbles() {
-}
-
 @MiniquestHandler(
     miniquest = Miniquest.LOST_HER_MARBLES,
     startText = "Speak to Darren Lightfinger in his cellar accessed through a trapdoor north of Lumbridge furnace.",
@@ -19,34 +15,23 @@ fun mapLostHerMarbles() {
     rewardsText = "5,000 Thieving XP.<br>Access to Dodgy Dereks's Dirty Deals store.<br>Access to wall safes.<br>Thieves' guild expansion.",
     completedStage = 5
 )
-
 class FromTinyAcorns : MiniquestOutline() {
     override fun getJournalLines(player: Player, stage: Int): List<String> {
-        val lines = ArrayList<String>()
-        when (stage) {
-            0 -> {
-                lines.add("I can start this miniquest by speaking to Darren Lightfinger in")
-                lines.add("the Lumbridge Thieves' Guild.")
-                lines.add("")
-            }
-
-            1 -> {
-                lines.add("Darren Lightfinger wants me to be on the lookout for valuable flame fragments,")
-                lines.add("which I should find while picking pockets around Gielinor.")
-                lines.add("")
-                lines.add("Robin can advise on their probable locations.")
-            }
-
-            5 -> {
-                lines.add("I found all thirty-two fragments and was rewarded with 9,800 Thieving XP,")
-                lines.add("a share of guild swag, and pointers on my pickpocketing technique.,")
-                lines.add("The Thieves' Guild's facilities have been expanded further.")
-                lines.add("")
-                lines.add("MINIQUEST COMPLETE!")
-            }
-            else -> lines.add("Invalid quest stage. Report this to an administrator.")
+        return when (stage) {
+            0 -> listOf("I can start this miniquest by speaking to Darren Lightfinger in the Lumbridge Thieves' Guild.")
+            1 -> listOf(
+                "Darren Lightfinger wants me to be on the lookout for valuable flame fragments, which I should find while picking pockets around Gielinor.",
+                "",
+                "Robin can advise on their probable locations."
+            )
+            5 -> listOf(
+                "I found all thirty-two fragments and was rewarded with 9,800 Thieving XP, a share of guild swag, and pointers on my pickpocketing technique.",
+                "The Thieves' Guild's facilities have been expanded further.",
+                "",
+                "MINIQUEST COMPLETE!"
+            )
+            else -> listOf("Invalid quest stage. Report this to an administrator.")
         }
-        return lines
     }
 
     override fun complete(player: Player) {
