bffd4785d8f54c81eb45c120e5ba429303379801
Trenton Kress <trenton.kress@gmail.com>
2024-08-31 17:42:06 -0700

    Finish firewall attack logic and emulate speed

--- src/main/java/com/rs/game/content/bosses/qbd_trent/QBD.kt
+++ src/main/java/com/rs/game/content/bosses/qbd_trent/QBD.kt
@@ -1,9 +1,15 @@
 package com.rs.game.content.bosses.qbd_trent
 
 import com.rs.engine.pathfinder.Direction
+import com.rs.game.World
 import com.rs.game.model.entity.Entity
+import com.rs.game.model.entity.async.schedule
 import com.rs.game.model.entity.npc.NPC
+import com.rs.game.model.entity.npc.combat.CombatScriptsHandler
+import com.rs.game.model.entity.npc.combat.NPCCombat
+import com.rs.game.model.entity.player.Player
 import com.rs.lib.game.Tile
+import com.rs.lib.util.Utils
 import com.rs.plugin.annotations.ServerStartupEvent
 import com.rs.plugin.kts.instantiateNpc
 import com.rs.plugin.kts.npcCombat
@@ -23,10 +29,12 @@ class QBD(npcId: Int, tile: Tile) : NPC(npcId, tile, Direction.SOUTH, true) {
 fun mapQbdFeatures() {
     instantiateNpc(15454, 15506, 15507, 15508, 15509) { npcId, tile -> QBD(npcId, tile) }
 
-    npcCombat(
-        15454, 15506, 15507, 15508, 15509,
-        script = ::qbdAttack
-    )
+    npcCombat(15454, 15506, 15507, 15508, 15509) { npc, target ->
+        val qbd = npc as? QBD
+        val player = target as? Player
+        if (qbd == null || player == null) return@npcCombat CombatScriptsHandler.getDefaultCombat().apply(npc, target)
+        return@npcCombat qbdAttack(qbd, player)
+    }
 }
 
 private const val MELEE_LEFT = 16743
@@ -43,7 +51,27 @@ private const val FIRE_WALL_CONTINUE = 16747
 private const val FIRE_WALL_STOP = 16748
 private const val WAKE_UP = 16848
 
-fun qbdAttack(npc: NPC, target: Entity): Int {
-    npc.anim(MELEE_CENTER)
-    return 5
+fun qbdAttack(qbd: QBD, player: Player): Int {
+    qbd.firewall(player, Utils.random(3))
+    return 30
+}
+
+private fun QBD.firewall(target: Player, variant: Int) {
+    val spotAnim = 3158+variant
+    var offset = 0
+    tasks.scheduleTimer(0, 0) { tick ->
+        when(tick) {
+            0 -> World.sendProjectile(tile.transform(2, 2), tile.transform(2, -18), spotAnim, 0 to 0, 0, 30, 0, 0)
+            15 -> return@scheduleTimer false
+            else -> {
+                offset++
+                val baseTile = tile.transform(2, 2-offset);
+                for (x in -10..10) {
+                    World.sendSpotAnim(baseTile.transform(x, 0), 502)
+                    World.sendSpotAnim(baseTile.transform(x, -1), 502)
+                }
+            }
+        }
+        return@scheduleTimer true
+    }
 }
