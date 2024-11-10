1aeb7dc381992a2d814f6a719822aaccad697f1d
MrAshieB <MrAshieB@users.noreply.github.com>
2024-11-10 19:24:08 +0000

    Gu'Tanoth: Add Bogrog's summoning obelisk functionality.

--- src/main/java/com/rs/game/content/world/areas/gu_tanoth/GuTanoth.kt
+++ src/main/java/com/rs/game/content/world/areas/gu_tanoth/GuTanoth.kt
@@ -1,6 +1,10 @@
 package com.rs.game.content.world.areas.gu_tanoth
 
+import com.rs.engine.quest.Quest
+import com.rs.game.content.quests.wolfwhistle.WolfWhistle
 import com.rs.game.content.skills.agility.Agility
+import com.rs.game.content.skills.summoning.Summoning
+import com.rs.game.model.entity.player.Skills
 import com.rs.plugin.annotations.ServerStartupEvent
 import com.rs.plugin.kts.onObjectClick
 
@@ -9,5 +13,30 @@ fun mapGuTanoth() {
     onObjectClick(2832) { e ->
         if (!Agility.hasLevel(e.player, 20)) return@onObjectClick
         Agility.handleObstacle(e.player, 3303, 2, e.player.transform(if (e.player.x < e.getObject().x) 2 else -2, 0, 0), 1.0)
+    }
+
+    onObjectClick(28722) { (player, obj, option) ->
+        when (option) {
+            "Infuse-pouch" -> {
+                if (player.questManager.getStage(Quest.WOLF_WHISTLE) == WolfWhistle.WOLPERTINGER_CREATION) {
+                    if (WolfWhistle.wolfWhistleObeliskReadyToInfusePouch(player)) {
+                        WolfWhistle.doWolpertingerPouchCreation(player, obj)
+                        return@onObjectClick
+                    }
+                }
+                Summoning.openInfusionInterface(player, false);
+            }
+            "Renew-points" -> {
+                val summonLevel = player.skills.getLevelForXp(Skills.SUMMONING);
+                if (player.skills.getLevel(Skills.SUMMONING) < summonLevel) {
+                    player.lock(3)
+                    player.anim(8502)
+                    player.skills.set(Skills.SUMMONING, summonLevel)
+                    player.sendMessage("You have recharged your Summoning points.", true)
+                    return@onObjectClick
+                }
+                player.sendMessage("You already have full Summoning points.")
+            }
+        }
     }
 }
